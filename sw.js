f1fa3805267efe0238c1b88611939c3186b55b5f
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2022-01-27 22:54:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  'https://ribalda.github.io/hello-pwa/',
+  'https://ribalda.github.io/hello-pwa/index.html',
+  'https://ribalda.github.io/hello-pwa/css/style.css',
+  'https://ribalda.github.io/hello-pwa/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
